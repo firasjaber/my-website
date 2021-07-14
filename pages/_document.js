42f869cedc200327f1d3581a79b2a39f910ac182
Firas Jaber <firasjaberc@gmail.com>
2021-07-14 14:34:53 +0100

    initial commit : website v1

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300;400&family=Varela+Round&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        varella: ['"Varela Round"', 'sans-serif'],
+        scode: ['"Source Code Pro"', 'sans-serif'],
+      },
+      keyframes: {
+        wiggle: {
+          '0%': { transform: 'rotate( 0.0deg)' },
+          '10%': {
+            transform: 'rotate(14.0deg)',
+          },
+          '20%': { transform: 'rotate(-8.0deg)' },
+          '30%': { transform: 'rotate(14.0deg)' },
+          '40%': { transform: 'rotate(-4.0deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': {
+            transform: 'rotate( 0.0deg)',
+          },
+          '100%': { transform: 'rotate( 0.0deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 2.5s infinite',
+      },
+      transformOrigin: {
+        70: '70% 70%',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
