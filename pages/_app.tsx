8a43fb698c58e870fb18bd06732fc964d0bf2464
Firas Jaber <firasjaberc@gmail.com>
2021-11-07 15:23:14 +0100

    added page progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import { useEffect, useRef } from 'react';
 import { loadCursor } from 'utils/cursor';
+import NextNProgress from 'nextjs-progressbar';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -18,6 +19,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div
         ref={ballCanvas}
         className='opacity-0 fixed ball-transitions duration-400 pointer-events-none z-30 h-6 w-6 bg-transparent border border-white rounded-full shadow-md'
+      />
+      <NextNProgress
+        color='#29D'
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
       />
       <Component {...pageProps} />
     </div>
